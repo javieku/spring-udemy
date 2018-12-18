922f1533c9eedcd6a2b441c8b8d3e0bf7f863f71
javieku <javieku@gmail.com>
2018-12-18 19:36:07 +0100

    Add basic spring mvc application

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
-	@RequestMapping("/showform")
+	@RequestMapping("/showForm")
 	public String showForm() {
 		return "helloworld-form";
 	}
@@ -26,7 +28,21 @@ public class HelloWorldController {
 		
 		theName = theName.toUpperCase();
 		
-		String result = "Yo! " + theName;
+		String result = "YO! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Hey My Friend from v3! " + theName;
 		
 		model.addAttribute("message", result);
 		
