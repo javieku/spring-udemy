a8bb6625f4d6da6092efa07eb6d123082fe5b659
javieku <javieku@gmail.com>
2018-12-20 16:48:30 +0100

    Add demo apps for hibernate eager vs lazy data loading

---
+++ hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/FetchJoinDemo.java
@@ -0,0 +1,56 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class FetchJoinDemo {
+
+	
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			session.beginTransaction();
+			
+			int id = 1;
+			Query<Instructor> query = 
+					session.createQuery("select i from Instructor i "
+							+ "JOIN FETCH i.courses "
+							+ "where i.id=:theInstructorId", 
+							Instructor.class);
+			
+			query.setParameter("theInstructorId", id);
+			
+			Instructor instructor = query.getSingleResult();
+			
+			System.out.println("Instructor: " + instructor);
+		
+			session.getTransaction().commit();
+			
+			session.close();
+			
+			System.out.println("Done!");
+			
+		}catch(Exception e ) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+	
+}
