ba746c9a8b3c888217bee1f1cdb993e78f3625d8
javieku <javieku@gmail.com>
2018-12-22 17:39:55 +0100

    Add exception handling to Spring REST example app

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +35,11 @@ public class StudentRestController {
 	
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId){
+		
+		if(studentId < 0 || studentId >= students.size()) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		
 		return students.get(studentId);	
 	}
 }
