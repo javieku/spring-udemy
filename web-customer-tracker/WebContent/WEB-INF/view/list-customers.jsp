7f4847c9c088306abe5de055787708bbaa51436c
javieku <javieku@gmail.com>
2018-12-22 14:00:38 +0100

    Add support for search, add, delete and update customer in the CRM web app

--- web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
+++ web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,18 +22,46 @@
 
 <div id="container">
 	<div id="content"> 
+		
+		<input type="button" value="Add Customer" 
+			   onClick="window.location.href='showFormForAdd'"
+			   class="add-button"
+		/>
+		
+		<form:form action="search" method="POST">
+			Search Customer: <input type="text" name="searchName"/>
+			<input type="submit" value="Search" class="add-button"/>
+		</form:form>
+	
 		<table>
 			<tr>
 				<th>First Name</th>
 				<th>Last Name </th>
 				<th>Email</th>
+				<th>Action</th>
 			</tr>
 			
 			<c:forEach var="tmpCustomer" items="${customers}">
+			
+				<c:url var="updateLink" value="/customer/showFormForUpdate">
+					<c:param name="customerId" value="${tmpCustomer.id}"/>
+				</c:url>
+				
+				<c:url var="deleteLink" value="/customer/delete">
+					<c:param name="customerId" value="${tmpCustomer.id}"/>
+				</c:url>
+				
 				<tr>
 					<td>${tmpCustomer.firstName}</td>
 					<td>${tmpCustomer.lastName}</td>
 					<td>${tmpCustomer.email}</td>
+					
+					<td>
+						<a href="${updateLink}">Update</a>
+						|
+						<a href="${deleteLink}"
+						   onclick="if(!(confirm('Are you sure you want to delete this customer?'))) return false">Delete</a>
+					</td>
 				</tr>
 			</c:forEach>
 		</table>
--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,22 +5,73 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		List<Customer> customers = customerDAO.getCustomers();
+		List<Customer> customers = customerService.getCustomers();
+
+		theModel.addAttribute("customers", customers);
+		
+		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		theModel.addAttribute("customer", new Customer());
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int id, 
+									Model theModel) {
+		
+		Customer customer = customerService.getCustomer(id);
+		
+		theModel.addAttribute("customer", customer);
+		
+		return "customer-form";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int id, 
+			Model theModel) {
+		
+		customerService.deleteCustomer(id);
+		
+		return "redirect:/customer/list";
+	}
+	
+	@PostMapping("/search")
+	public String searchCustomer(@RequestParam("searchName") String name, 
+			Model theModel) {
+		
+		List<Customer> customers = customerService.searchCustomer(name);
 
 		theModel.addAttribute("customers", customers);
 		
