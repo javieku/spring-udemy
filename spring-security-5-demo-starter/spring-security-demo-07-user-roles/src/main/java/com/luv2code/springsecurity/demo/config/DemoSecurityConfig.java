bfcca51c45e617d86130b2726e8dd42ffea7b614
javieku <javieku@gmail.com>
2019-02-10 20:32:02 +0100

    Add project to practise role handling

---
+++ spring-security-5-demo-starter/spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("mary").password("test123").roles("MANAGER", "EMPLOYEE"))
+                .withUser(users.username("susan").password("test123").roles("ADMIN", "EMPLOYEE"));
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and()
+                                .formLogin()
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .loginPage("/showMyLoginPage")
+                                .permitAll()
+                                .and()
+                                .logout()
+                                .permitAll();
+    }
+
+}
